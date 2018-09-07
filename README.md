3fe292f09db8fe4da1c4f49d489c6c9bcd899ebc
Matthew Mantle <scommm2@AD.HUD.AC.UK>
2018-09-07 14:52:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Data and Variables
+* Clone the repository or click the download button (it's a big green button)
+* Unzip the folder
+* Open the .js file in a text editor
+* Open the HTML page in a browser
+	- Open the console.log to view output from the JavaScript programs
+* Add code to the .js file to answer questions
+* 
---
+++ js/wk13-prac.js
@@ -0,0 +1,29 @@
+/*
+1.	Write a JavaScript program prompts the user for their name and their favourite colour. 
+a)	The program should then output this information using a console.log(). 
+b)	Modify the program so that instead of a console.log(), it changes the background colour of the page to the colour the user has chosen. 
+*/
+
+/*
+2.	Write a JavaScript program that will 
+a)	Ask the user for two numbers, calculates the total of the two numbers and displays the total for the user. 
+b)	Modify the above code so that will work out the average of these two scores and display this value for the user.
+*/
+
+/*
+3.	Write a program that will convert a measurement in inches to centimetres (1 inch is 2.54 centimetres).
+*/
+
+/*
+4.	Create a 'design a page' application. A series of prompt boxes should ask the user for two colours, and their favourite font .  The content of the page should then be displayed in the user’s choice of colour and font. The background colour of the page should be change to the user’s second choice of colour. 
+*/
+
+/*
+5.	The module CFT2111 is assessed by two pieces of coursework. An HTML portfolio weighted at 40% and a JavaScript coursework weighted at 60%. Write a JavaScript application that will ask the user for their two coursework scores. The program should then feedback to the user with an overall percentage for the module, here’s some example input and output for the program. 
+
+What is your HTML coursework score? 56
+What is your JavaScript coursework score? 42
+Your overall mark for the module is 47.6
+
+Have a look on https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/round for information on how to round numbers.
+*/
