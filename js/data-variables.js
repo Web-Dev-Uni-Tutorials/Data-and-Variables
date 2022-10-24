28e2c266630ed342d39d222f8d8066af1ee85549
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-10-24 13:36:53 +0100

    Update data-variables.js

--- js/data-variables.js
+++ js/data-variables.js
@@ -1,36 +1,43 @@
-/*
-1. The following JavaScript program prompts the user for their name and then displays the user's name using a console.log().
-a) Add another line of code that will prompt the user for their favourite colour. Change the console.log statement so that it also displays the user's choice of colour e.g. 'Your name is Matthew. Your favourite colour is red'.
-b) Add another line of code that will change the background colour of the page to the colour the user has chosen.
-*/
+
+// 1. The following JavaScript program prompts the user for their name and then displays the user's name using a console.log().
+// a) Add another line of code that will prompt the user for their favourite colour. Change the console.log statement so that it also displays the user's choice of colour e.g. 'Your name is Matthew. Your favourite colour is red'.
+// b) Add another line of code that will change the background colour of the page to the colour the user has chosen.
+
 
 const userName=prompt("What is your name?");
 console.log(`Your name is ${userName}.`);
 
-/*
-2. The following JavaScript program asks the user for two numbers and outputs these numbers using a console.log. Uncomment the program by removing the two forward slashes at the start of each line. Run the program in a browser to check it works. 
-a) Modify this program so that it calculates the total of the two numbers, and displays this total for the user using a console.log().
-b) Add some additional code that will calculate the average of these two numbers and display this for the user.
-*/
+
+// 2. The following JavaScript program asks the user for two numbers and outputs these numbers using a console.log. 
+//    Uncomment the program by removing the two forward slashes at the start of each line. 
+//    Run the program in a browser to check it works. 
+// a) Modify this program so that it calculates the total of the two numbers, and displays this total for the user using a console.log().
+// b) Add some additional code that will calculate the average of these two numbers and display this for the user.
+
 
 // const num1=parseInt(prompt("Tell me a number?"));
 // const num2=parseInt(prompt("Tell me another number"));
 // console.log(`You entered ${num1} and ${num2}.`)
 
-/*
-3. Write a program that will convert a measurement in inches to centimetres (1 inch is 2.54 centimetres).
-*/
 
-/*
-4. Create a 'design a page' application. A three prompt boxes should ask the user for two colours, and their favourite font.  The content of the page should then be displayed in the user's choice of colour and font. The background colour of the page should change to the user's second choice of colour.
-*/
+// 3. Write a program that will convert a measurement in inches to centimetres (1 inch is 2.54 centimetres).
 
-/*
-5.The module CFT2111 has two pieces of assessment. HTML quizzes weighted at 40% and a JavaScript coursework weighted at 60%. Write a JavaScript application that will ask the user for their two scores. The program should then feedback to the user with an overall percentage for the module, here's some example input and output for the program.
 
-What is your HTML assessment score? 56
-What is your JavaScript assessment score? 42
-Your overall mark for the module is 47
 
-Have a look on https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/round for information on how to round numbers.
-*/
+// 4. Create a 'design a page' application. A three prompt boxes should ask the user for two colours, and their favourite font.  
+//    The content of the page should then be displayed in the user's choice of colour and font. 
+//    The background colour of the page should change to the user's second choice of colour.
+
+
+
+//5.The module CFT2111 has two pieces of assessment. 
+// HTML quizzes weighted at 40% and a JavaScript coursework weighted at 60%. 
+// Write a JavaScript application that will ask the user for their two scores. 
+//The program should then feedback to the user with an overall percentage for the module, here's some example input and output for the program.
+
+//What is your HTML assessment score? 56
+//What is your JavaScript assessment score? 42
+//Your overall mark for the module is 47
+
+//Have a look on https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Math/round for information on how to round numbers.
+
